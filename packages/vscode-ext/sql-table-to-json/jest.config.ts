2a7f53677c48252b69483b52020d7ac85c4a9931
Keyur Shah <keyurshah3939@gmail.com>
2023-03-09 14:17:05 +0530

    Nx Bump

--- packages/vscode-ext/sql-table-to-json/jest.config.ts
+++ packages/vscode-ext/sql-table-to-json/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'vscode-ext-sql-table-to-json',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../coverage/packages/vscode-ext/sql-table-to-json',
